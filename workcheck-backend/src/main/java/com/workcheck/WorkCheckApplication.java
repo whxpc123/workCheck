82d4d728b12469d261b68af7d43b3e0274f67004
xiangpc <wh_xpc@163.com>
2025-12-10 08:59:51 +0800

    测试提交

--- workcheck-backend/src/main/java/com/workcheck/WorkCheckApplication.java
+++ workcheck-backend/src/main/java/com/workcheck/WorkCheckApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class WorkCheckApplication {
+    //新增项目
     public static void main(String[] args) {
         SpringApplication.run(WorkCheckApplication.class, args);
     }
